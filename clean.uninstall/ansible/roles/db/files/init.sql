95c15a7915cfcd2078d93b30af47530949e62190
lilongen <lilongen@gmail.com>
2018-08-17 19:08:09 +0800

    1

---
+++ clean.uninstall/ansible/roles/db/files/init.sql
@@ -0,0 +1,20 @@
+drop database if exists cdh_hive;
+drop database if exists cdh_activitymonitor;
+drop database if exists cdh_reportsmanager;
+drop database if exists cdh_oozie;
+drop database if exists cdh_hue;
+
+create database cdh_hive default character set utf8;
+create database cdh_activitymonitor default character set utf8;
+create database cdh_reportsmanager default character set utf8;
+create database cdh_oozie default character set utf8;
+create database cdh_hue default character set utf8;
+
+delete from mysql.user where mysql.user.user like 'cdh_%';
+grant all privileges on cdh_hive.* to cdh_hive@'%' identified by 'uydc.cmf' with grant option;
+grant all privileges on cdh_activitymonitor.* to cdh_activitymonitor@'%' identified by 'uydc.cmf' with grant option;
+grant all privileges on cdh_reportsmanager.* to cdh_reportsmanager@'%' identified by 'uydc.cmf' with grant option;
+grant all privileges on cdh_oozie.* to cdh_oozie@'%' identified by 'uydc.cmf' with grant option;
+grant all privileges on cdh_hue.* to cdh_hue@'%' identified by 'uydc.cmf' with grant option;
+
+flush privileges;
