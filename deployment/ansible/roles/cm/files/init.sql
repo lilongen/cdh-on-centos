eec0fa166610d454f0dc8d827df29af20c026a78
lilongen <lilongen@gmail.com>
2018-08-20 13:57:45 +0800

    1

--- deployment/ansible/roles/cm/files/init.sql
+++ deployment/ansible/roles/cm/files/init.sql
@@ -1,3 +1,5 @@
+delete from mysql.user where mysql.user.user like 'cdh_%';
+
 drop database if exists cdh_hive;
 drop database if exists cdh_activitymonitor;
 drop database if exists cdh_reportsmanager;
@@ -10,7 +12,6 @@ create database cdh_reportsmanager default character set utf8;
 create database cdh_oozie default character set utf8;
 create database cdh_hue default character set utf8;
 
-delete from mysql.user where mysql.user.user like 'cdh_%';
 grant all privileges on cdh_hive.* to cdh_hive@'%' identified by 'uydc.cmf' with grant option;
 grant all privileges on cdh_activitymonitor.* to cdh_activitymonitor@'%' identified by 'uydc.cmf' with grant option;
 grant all privileges on cdh_reportsmanager.* to cdh_reportsmanager@'%' identified by 'uydc.cmf' with grant option;
