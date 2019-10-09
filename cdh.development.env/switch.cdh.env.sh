3676dfe7b81f6dcdca5a80ae1b8f5ef01d937883
lilongen <lilongen@gmail.com>
2019-10-09 16:31:14 +0800

    cdh spark client env switcher is function ready

--- cdh.development.env/switch.cdh.env.sh
+++ cdh.development.env/switch.cdh.env.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#
 
 to=$1
 
@@ -6,37 +7,53 @@ spark_dist_classpath_file="/opt/cloudera/spark.dist.classpath/${to}/classpath.tx
 spark_defaults_file="/opt/cloudera/spark.defaults/${to}/spark-defaults.conf"
 hadoop_conf_dir="/opt/cloudera/hadoop.conf/${to}"
 
-declare -A path_appendix 
-path_appendix=( \
-    ["test"]="/opt/cloudera/parcels/CDH-6.3.0-1.cdh6.3.0.p0.1279813/bin" \
-    ["dev"]="/opt/cloudera/parcels/CDH-5.14.0-1.cdh5.14.0.p0.24/bin" \
-    ["dev"]="/opt/cloudera/parcels/CDH-5.14.0-1.cdh5.14.0.p0.24/bin" \
+declare -A cdh_parcels 
+cdh_parcels=( \
+    ["test"]="/opt/cloudera/parcels/CDH-6.3.0-1.cdh6.3.0.p0.1279813" \
+    ["dev"]="/opt/cloudera/parcels/CDH-5.14.0-1.cdh5.14.0.p0.24" \
+    ["dev"]="/opt/cloudera/parcels/CDH-5.14.0-1.cdh5.14.0.p0.24" \
 )
+cdh_parcel=${cdh_parcels[$to]}
 
 echo_ln_cdh() {
-    bin_path=${path_appendix[$to]}
-    cdh_path=${bin_path:0:-4}
-    echo ln -sf $cdh_path /opt/cloudera/parcels/CDH
+    echo ln -sf ${cdh_parcel} /opt/cloudera/parcels/CDH
+}
+
+# 1. set JVM_D_JAVA_SECURITY_KRB5_CONF=-Djava.security.krb5.conf=/path/to/krb5.conf
+# 2. inject ${JVM_D_JAVA_SECURITY_KRB5_CONF} into spark-submit shell
+# 3. ${JVM_D_JAVA_SECURITY_KRB5_CONF} export in echo_env_exports 
+echo_inject_jvmd_krb5conf_to_spark_submit() {
+    spark_submit_file="${cdh_parcel}/lib/spark/bin/spark-submit"
+    echo "perl -i -pE 's|(/bin/spark-class).+(org.apache.spark.deploy.SparkSubmit)|\${1} \\\${JVM_D_JAVA_SECURITY_KRB5_CONF} \${2}|m' $spark_submit_file"
 }
 
 echo_env_exports() {
     spark_dist_classpath="$(paste -sd: ${spark_dist_classpath_file})"
     spark_yarn_jar=$(grep spark.yarn.jar ${spark_defaults_file} | awk -F'=' '{print $2}')
+    krb5_conf_file="/opt/cloudera/krb5/${to}/krb5.conf"
     echo export HADOOP_CONF_DIR=\"${hadoop_conf_dir}\"
-    echo export PATH=\"${path_appendix[$to]}:$PATH\"
+    echo export PATH=\"${cdh_parcel}/bin:$PATH\"
+    echo export SPARK_DEFAULTS=\"${spark_defaults_file}\"
     echo export SPARK_YARN_JAR=\"$spark_yarn_jar\"
     echo export SPARK_DIST_CLASSPATH=\"$spark_dist_classpath\"
+    echo export KRB5_CONFIG=\"${krb5_conf_file}\"
+    
+    # Why need JVM_D_JAVA_SECURITY_KRB5_CONF? 
+    # 1). spark-submit, can not using environment variable KRB5_CONFIG
+    # 2). spark-submit -> java ...
+    # 3). append -Djava.security.krb5.conf=/path/to/krb5.conf into spark-submit command
+    echo export JVM_D_JAVA_SECURITY_KRB5_CONF=\"-Djava.security.krb5.conf=${krb5_conf_file}\"
 }
 
-echo_lnkrb5conf_and_kinit() {
-    echo sudo ln -sf /opt/cloudera/krb5/$to/krb5.conf /etc/krb5.conf
+echo_kinit() {
     echo kinit -kt /opt/cloudera/keytab/$to/lile.keytab lile
 }
 
 main() {
-    echo_ln_cdh
+#    echo_ln_cdh
     echo_env_exports
-    echo_lnkrb5conf_and_kinit
+    echo_inject_jvmd_krb5conf_to_spark_submit
+    echo_kinit
 }
 
 main
